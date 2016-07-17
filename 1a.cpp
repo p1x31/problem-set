3413dcd01b1afc1eb0f6a4cea2bc389a8af94292
Vadim Makarenkov <vadim_makarenkov@mail.ru>
2016-07-17 13:57:57 +0000

    1A

---
+++ 1a.cpp
@@ -0,0 +1,16 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n, m, a;
+    cin >> n >> m >> a;
+    long long k = ceil((double) n/a);
+    long long l = ceil((double) m/a);
+    cout << k * l;
+    //cout << k << " " << l;
+    
+    
+}
